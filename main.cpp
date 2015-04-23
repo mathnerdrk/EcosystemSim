8fc614d0901f2b11d8ea9922377a490068ad55df
mathnerdrk <rahul.krishnan@hotmail.com>
2015-04-22 20:50:49 -0600

    Create main.cpp + stuff
    stuff = libraries, main method, comments (for organization), and examples!!!

---
+++ main.cpp
@@ -0,0 +1,30 @@
+#include<iostream> //basic c++ library
+#include<fstream> // file IO library
+#include<string> // string library
+#include<vector> //vector library
+//can add more #includes if need more libraries
+
+using namespace std;
+
+//global variables:
+
+//classes:
+
+//method declarations:
+/*example:
+void nando(int overachievinglevel, int richlevel);
+*/
+
+int main()
+{
+  
+  return 0;
+}
+
+//method bodies:
+/*example:
+void nando(int overachievinglevel, int richlevel)
+{
+  //do stuff to nando
+}
+*/
